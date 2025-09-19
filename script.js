2a997b9688e8467d032cbea9893d5beb8df29758
Sukimo <78065507+Sukimo@users.noreply.github.com>
2025-09-19 13:11:15 +0700

    update project-cards at projects section

--- script.js
+++ script.js
@@ -8,4 +8,20 @@ function toggleSection(sectionId) {
   if (activeSection) {
     activeSection.classList.add('active');
   }
+  
+  //hide moblie nav after clicking
+  const nav=document.getElementById('navMenu');
+  if(window.innerWidth <=600){
+  nav.classList.remove('show');
+  }
 }
+
+//toggle hamburger menu
+const hamburger =document.getElementById('hamburger');
+const navMenu =document.getElementById('navMenu');
+
+hamburger.addEventListener('click', ()=>{
+  navMenu.classList.toggle('show');
+});
+
+
